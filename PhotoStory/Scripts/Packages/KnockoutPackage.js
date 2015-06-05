77762336b71fee244e4acb7b1938b9cbc3b7995c
Michael Fry <michaelfry2002@gmail.com>
2015-06-05 19:10:26 +1200

    asdas

---
+++ PhotoStory/Scripts/Packages/KnockoutPackage.js
@@ -0,0 +1,97 @@
+﻿define(function (require) {
+
+	var $ = require("jquery"),
+		ko = require("knockout"),
+		komapping = require("knockoutmapping");
+
+	var knockoutPackage = {};
+	knockoutPackage.$ = $;
+	knockoutPackage.ko = ko;
+	knockoutPackage.komapping = komapping;
+
+	ko.bindingHandlers.valueOrPlaceholder = {
+		init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+			ko.bindingHandlers.value.init(element, valueAccessor, allBindings, viewModel, bindingContext);
+		},
+
+		update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+			var value = valueAccessor(),
+				text = value() || element.placeholder;
+			value(text);
+			ko.bindingHandlers.value.update(element, valueAccessor, allBindings, viewModel, bindingContext);
+		}
+	};
+
+	// Gives access to the element, for integration with non-Knockout operations
+	ko.bindingHandlers.element = {
+		update: function (element, valueAccessor) {
+			if (!this._hasBeenUpdated) {
+				valueAccessor()(element);
+				this._hasBeenUpdated = true;
+			}
+		}
+	};
+
+	ko.bindingHandlers.bootstrapFileInput = {
+		init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+			require(["fileinput"], function () {
+				var fileInput = $(element).fileinput({
+					uploadUrl: "http://localhost/file-upload-batch/1", // server upload action
+					uploadAsync: true,
+					maxFileCount: 20,
+					previewFileType: "image",
+					previewSettings: {
+						image: { width: "120px", height: "auto" }
+					},
+					layoutTemplates: {
+						actions:
+								'<div class="file-actions">\n' +
+								'    <div class="file-footer-buttons">\n' +
+								'        {delete}' +
+								'    </div>\n' +
+								'    <div class="file-upload-indicator" tabindex="-1" title="{indicatorTitle}">{indicator}</div>\n' +
+								'    <div class="clearfix"></div>\n' +
+								'</div>'
+					}
+				});
+
+				valueAccessor()($(fileInput).data('fileinput'));
+			});
+		}
+	};
+
+	ko.bindingHandlers.fileInput = {
+		init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+			var value = valueAccessor(),
+				name = value.name,
+				uploadUrl = value.uploadUrl,
+				maxFileCount = isNaN(value.maxFileCount) ? 5 : value.maxFileCount;
+
+			require(["fileinput"], function () {
+				$(element).fileinput({
+					uploadAsync: true,
+					maxFileCount: maxFileCount,
+					uploadUrl: uploadUrl,
+					previewFileType: "image",
+					previewSettings: {
+						image: { width: "120px", height: "auto" }
+					},
+					layoutTemplates: {
+						actions:
+								'<div class="file-actions">\n' +
+								'    <div class="file-footer-buttons">\n' +
+								'        {delete}' +
+								'    </div>\n' +
+								'    <div class="clearfix"></div>\n' +
+								'</div>'
+					}
+				});
+			});
+			
+
+			viewModel[name](value);
+		}
+	};
+
+	return knockoutPackage;
+});
