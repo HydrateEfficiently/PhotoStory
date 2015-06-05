37fe8c42ccc1aee9c68d3a3b78939bc0f7572ec7
Michael Fry <michaelfry2002@gmail.com>
2015-06-05 18:51:03 +1200

    Save

--- PhotoStory/Scripts/ViewModels/Stories/Chapter_New.js
+++ PhotoStory/Scripts/ViewModels/Stories/Chapter_New.js
@@ -6,30 +6,8 @@
 
 	function Chapter_New() {
 		var self = this;
-		this.FileInputHandle = ko.observable();
-		// this.FileInputHandle.subscribe(function (element) {
-		// 	require(["fileinput"], function () {
-		// 		self.FileInput = $(element).fileinput({
-		// 				uploadUrl: "http://localhost/file-upload-batch/1", // server upload action
-		// 				uploadAsync: true,
-		// 				maxFileCount: 20,
-		// 				previewFileType: "image",
-		// 				previewSettings: {
-		// 					image: { width: "120px", height: "auto" }
-		// 				},
-		// 				layoutTemplates: {
-		// 					actions:
-		// 							'<div class="file-actions">\n' +
-		// 							'    <div class="file-footer-buttons">\n' +
-		// 							'        {delete}' +
-		// 							'    </div>\n' +
-		// 							'    <div class="file-upload-indicator" tabindex="-1" title="{indicatorTitle}">{indicator}</div>\n' +
-		// 							'    <div class="clearfix"></div>\n' +
-		// 							'</div>'
-		// 			}
-		// 		}).data("fileinput");
-		// 	});
-		// });
+		this.Title = ko.observable();
+
 
 		this.test = ko.observable();
 	}
